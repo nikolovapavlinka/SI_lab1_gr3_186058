64d64cd4b418dee1dca7814bf143010641ec6816
Pavlinka Nikolova <pavlinka.nikolova@students.finki.ukim.com>
2020-04-09 00:51:06 +0200

    taxReturn method finished

--- Item.java
+++ Item.java
@@ -1,3 +1,4 @@
+
 class Item {
 	int id;
 	String name;
@@ -7,10 +8,36 @@ class Item {
 	char danok;
 
 	//TODO constructor
-
+	Item(int id, String name, double price)
+	{
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
 	//TODO setters and getters
+	public int getId() {
+		return this.id;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public double getPrice() {
+		return this.price
+	}
+	
+	public void setId(int idd) {
+		this.id = idd;
+	}
+	public void setName(int nm) {
+		this.name = nm;
+	}
+	public void setPrice(int pr) {
+		this.price = pr;
+	}
+	
 
 	double taxReturn () {
 		//TODO
+		return price/100 * 15
 	}
 }
